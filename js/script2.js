465a110cc258e7fbff9cb24c8ef19b33aed48c4f
Iryna Abrosimova <iryna.abrosimova@cxdisruption.com>
2022-12-28 14:55:21 +0100

    add second animation js

--- js/script2.js
+++ js/script2.js
@@ -1,6 +1,51 @@
 const dateTitle = document.querySelector(".cover-date");
+const navButton = document.querySelector(".nav-button");
+const navOpen = document.querySelector(".nav-open");
 
 const date = new Date();
 dateTitle.innerHTML = `${
   date.getMonth() + 1
 } / ${date.getDate()} / ${date.getFullYear()}`;
+
+const tl = gsap.timeline({
+  default: { duration: 1, ease: "power2.easeOut" },
+  paused: true,
+  reversed: true,
+});
+tl.to(".cover", {
+  width: "60%",
+})
+  .to(
+    "nav",
+    {
+      height: "100vh",
+    },
+    "-=0.5"
+  )
+  .fromTo(
+    ".nav-open",
+    {
+      opacity: 0,
+      x: 50,
+    },
+    {
+      opacity: 1,
+      x: 0,
+      onComplete: function () {
+        navOpen.style.pointerEvents = "auto";
+        console.log("done");
+      },
+    }
+  );
+navButton.addEventListener("click", (e) => {
+  if (tl.isActive()) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    return false;
+  }
+  toggle(tl);
+});
+
+function toggle(tween) {
+  tween.reversed() ? tween.play() : tween.reverse();
+}
